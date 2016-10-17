93352a0e0fe3fea9834f57ce5e3defdad96be182
Matthew McEachen <matthew+github@mceachen.org>
2016-10-17 12:04:51 -0700

    boom

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import * as ex from 'ts2-example/lib'
+import * as ex from 'ts2-example'
 
 const s = new ex.Student('Chuckles', 'Humphrey', 'McSnorty')
 const t = new ex.Teacher('Charlotte', 'Furbrawl')
+t.addStudent(s)
 
 ex.helloWorld()
+
+console.log(`Student: ${s}`)
+console.log(`Teacher: ${t}`)
